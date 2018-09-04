cb05492259012f57751593da3c32ee1d205fb42b
Allan <allanalvesantunes@gmail.com>
2018-09-04 00:31:43 -0300

    .

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -112,12 +112,12 @@ angular.module('app.controllers', [])
     // })
 
     .controller('feirasCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-    // You can include any angular dependencies as parameters for this function
-    // TIP: Access Route Parameters for your page via $stateParams.parameterName
-    function ($scope, $stateParams) {
+        // You can include any angular dependencies as parameters for this function
+        // TIP: Access Route Parameters for your page via $stateParams.parameterName
+        function ($scope, $stateParams) {
 
 
-    }])
+        }])
 
     .controller('qRCODECtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
         // You can include any angular dependencies as parameters for this function
